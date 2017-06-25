73f4b11c80e18ba0c599e86be962b12d38d068ac
Priyanka Advani <padvani89@gmail.com>
2017-06-25 05:00:54 +0000

    Complete skills assessment

---
+++ query.py
@@ -0,0 +1,125 @@
+"""
+
+This file is the place to write solutions for the
+skills assignment called skills-sqlalchemy. Remember to
+consult the exercise instructions for more complete
+explanations of the assignment.
+
+All classes from model.py are being imported for you
+here, so feel free to refer to classes without the
+[model.]User prefix.
+
+"""
+
+from model import *
+
+init_app()
+
+
+# -------------------------------------------------------------------
+# Part 2: Discussion Questions
+
+
+# 1. What is the datatype of the returned value of
+# ``Brand.query.filter_by(name='Ford')``?
+
+# The datatype returned is a query instance (specifically, an instance of the
+# flask_sqlalchemy.BaseQuery class)
+
+
+
+# 2. In your own words, what is an association table, and what type of
+# relationship (many to one, many to many, one to one, etc.) does an
+# association table manage?
+
+# An association table manages a many to many relationship by converting
+# it into two many to one relationships. Typically, the only columns are a
+# primary key and two foreign keys, each referencing one of the tables it
+# 'glues' together.
+
+
+
+
+# -------------------------------------------------------------------
+# Part 3: SQLAlchemy Queries
+
+
+# Get the brand with the brand_id of ``ram``.
+q1 = Brand.query.filter_by(brand_id="ram").one()
+
+# Get all models with the name ``Corvette`` and the brand_id ``che``.
+q2 = Model.query.filter_by(name='Corvette', brand_id='che').all()
+
+# Get all models that are older than 1960.
+q3 = Model.query.filter(Model.year < 1960).all()
+
+# Get all brands that were founded after 1920.
+q4 = Brand.query.filter(Brand.founded > 1920).all()
+
+# Get all models with names that begin with ``Cor``.
+q5 = Model.query.filter(Model.name.like('Cor%')).all()
+
+# Get all brands that were founded in 1903 and that are not yet discontinued.
+q6 = Brand.query.filter_by(founded=1903, discontinued=None).all()
+
+# Get all brands that are either 1) discontinued (at any time) or 2) founded
+# before 1950.
+q7 = Brand.query.filter(db.or_(Brand.discontinued != None, Brand.founded < 1950)).all()
+
+# Get all models whose brand_id is not ``for``.
+q8 = Model.query.filter(Model.brand_id != 'for').all()
+
+
+
+# -------------------------------------------------------------------
+# Part 4: Write Functions
+
+
+def get_model_info(year):
+    """Takes in a year and prints out each model name, brand name, and brand
+    headquarters for that year using only ONE database query."""
+
+    model_info = (db.session.query(Model.name, Brand.name, Brand.headquarters)
+                  .join(Brand).filter(Model.year == year).all())
+
+    for model, brand, hq in model_info:
+        print model + "\n" + brand + "\n" + hq + "\n"
+
+
+def get_brands_summary():
+    """Prints out each brand name (once) and all of that brand's models,
+    including their year, using only ONE database query."""
+
+    brand_models = db.session.query(Brand.name, Model.name, Model.year).outerjoin(Model).all()
+
+    brand_models_dict = {}
+
+    for brand, model, year in brand_models:
+        brand_models_dict[brand] = brand_models_dict.get(brand, [])
+        if model:
+            brand_models_dict[brand].append((model, year))
+
+    for brand, model_info in brand_models_dict.items():
+        if model_info :
+            print brand
+            for model, year in model_info:
+                print model, year
+            print "\n"
+        else:
+            print brand
+            print "\n"
+
+
+def search_brands_by_name(mystr):
+    """Returns all Brand objects corresponding to brands whose names include
+    the given string."""
+
+    return Brand.query.filter(Brand.name.ilike('%'+mystr+'%')).all()
+
+
+def get_models_between(start_year, end_year):
+    """Returns all Model objects corresponding to models made between
+    start_year (inclusive) and end_year (exclusive)."""
+
+    return Model.query.filter((Model.year >= start_year) & (Model.year < end_year)).all()
+
